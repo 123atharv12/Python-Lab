8b29ed89fcbc7bc2b7343b5437c64c518c5d4b32
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-11-22 14:16:08 +0530

    Q.11

---
+++ Q.11.py
@@ -0,0 +1,9 @@
+#Atharv Tamrakar
+
+# Python program to convert lowercase to uppercase
+
+#take input
+string = input('Enter any string: ')
+
+# upper() function to convert lowercase to uppercase
+print('In Upper Case:', string.upper())
