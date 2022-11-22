23bee9fcc5781252f5ff68e0f33735e87093c65b
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-11-22 14:15:28 +0530

    Q.10

---
+++ Q.10.py
@@ -0,0 +1,16 @@
+#Atharv Tamrakar
+List = []
+print("Blank List: ")
+print(List)
+  
+# Creating a List of numbers
+List = [10, 20, 14]
+print("\nList of numbers: ")
+print(List)
+  
+# Creating a List of strings and accessing
+# using index
+List = ["S.V polytechnic", "For", "college"]
+print("\nList Items: ")
+print(List[0])
+print(List[2])
