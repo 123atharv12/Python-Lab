6a8b100106c613bb1c254dfe1df43b6b23f113b0
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-11-22 14:10:52 +0530

    Q.7

---
+++ Q.7.py
@@ -0,0 +1,19 @@
+#Atharv Tamrakar
+def PrimeChecker(a):  
+    
+    if a > 1:  
+        
+        for j in range(2, int(a/2) + 1):  
+          
+            if (a % j) == 0:  
+                print(a, "is not a prime number")  
+                break  
+        
+        else:  
+            print(a, "is a prime number")  
+     
+    else:  
+        print(a, "is not a prime number")  
+a = int(input("Enter an input number:"))  
+
+PrimeChecker(a)  
