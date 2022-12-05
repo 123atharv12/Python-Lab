7afe405ab7f008346e7b8eb2e9d65cf4a0856de7
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-12-05 20:25:05 +0530

    Q20

---
+++ Q.20.py
@@ -0,0 +1,18 @@
+#Atharv Tamrakar
+
+# Python program to find difference between two numbers
+
+# first number
+num1 = 150
+# second number
+num2 = 50
+
+# num1 is greater than num2
+if num1 > num2:
+    diff = num1 - num2
+# num1 is less than num2
+else:
+    diff = num2 - num1
+
+# print difference value
+print('The difference between numbers =', diff)
