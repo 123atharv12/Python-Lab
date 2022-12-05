4143a6d958e4da4204103c861570f2a181690149
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-12-05 19:55:47 +0530

    Q.13

---
+++ Q.13.py
@@ -0,0 +1,12 @@
+#Atharv Tamrakar
+
+# This program adds two numbers
+
+num1 = 2.4
+num2 = 3.2
+
+# Add two numbers
+sum = num1 + num2
+
+# Display the sum
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
