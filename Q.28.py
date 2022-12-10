9885ad974e1a93708365bfb59b6c41595a05e39f
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-12-10 13:41:15 +0530

    Add files via upload

---
+++ Q.28.py
@@ -0,0 +1,4 @@
+#AtharvTamrakar
+a=int(input("Enter Number : "))
+for i in range(1,11):
+    print(a,"x",i, "=", a*i)
---
+++ Q.29.py
@@ -0,0 +1,4 @@
+#AtharvTamrakar
+a=int(input("Enter Height of Triangle : "))
+b=int(input("Enter Base of triangle : "))
+print((a*b)/2)
