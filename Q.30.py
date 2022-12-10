08f94f8c54b4918a9ddf132f7d9e5fb22b4a8668
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-12-10 13:43:20 +0530

    Add files via upload

---
+++ Q.30.py
@@ -0,0 +1,6 @@
+#AtharvTamrakar
+a=float(input("Enter the Temperature in Fahrenheit : "))
+b=a/32.8
+print(b)
+
+
---
+++ Q.31.py
@@ -0,0 +1,5 @@
+#AtharvTamrakar
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+c=int(input("Enter third number : "))
+print("The average of these numbers are : ", (a+b+c)/3)
