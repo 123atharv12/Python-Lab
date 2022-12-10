f2200ad6baa0608043dcb65b590a772be7d4f357
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-12-10 13:39:42 +0530

    Add files via upload

---
+++ Q.27.py
@@ -0,0 +1,10 @@
+#AtharvTamrakar
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+c=int(input("Enter third number : "))
+if a<b & a<c:
+    print(a,"is the smallest number")
+if b<a & b<c:
+    print(b,"is the smallest number")
+if c<a & c<b:
+    print(c,"is the smallest number")
