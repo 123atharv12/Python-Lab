83780ebc871bad178f438b9977d4226868b88341
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-12-10 13:47:30 +0530

    Add files via upload

---
+++ Q.35.py
@@ -0,0 +1,4 @@
+#AtharvTamrakar
+a=int(input("Enter first number : "))
+b=int(input("Enter second number : "))
+print(a-b)
---
+++ Q.36.py
@@ -0,0 +1,8 @@
+#AtharvTamrakar
+a=int(input("Enter the value of a : "))
+b=int(input("Enter the value of b : "))
+c=a
+a=b
+b=c
+print("The value of a is : ", a)
+print("The value of b is : ", b)
