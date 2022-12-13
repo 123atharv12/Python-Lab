0ede1498e8bc73ff6888764e72d74f9fadb87d78
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-12-13 14:27:20 +0530

    Add files via upload

---
+++ Q.39.py
@@ -0,0 +1,4 @@
+#AtharvTamrakar
+a=int(input("Enter the Temperature in Celcius :- "))
+b=a+273.15
+print("The temperature in Kelvin is :- ", b)
---
+++ Q.40.py
@@ -0,0 +1,6 @@
+#AtharvTamrakar
+a=int(input("Enter Number : "))
+if a%2==0:
+    print("Even Number")
+else:
+    print("Odd Number")
