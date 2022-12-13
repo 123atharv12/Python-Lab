5942716f144029567dbdec401f5c367cd1e08d8d
Atharv Tamrakar <100550088+123atharv12@users.noreply.github.com>
2022-12-13 14:33:18 +0530

    Add files via upload

---
+++ Q.53.py
@@ -0,0 +1,5 @@
+a=int(input("Enter Principle Amount : "))
+b=int(input("Enter Rate of Interest : "))
+c=int(input("Enter Time : "))
+d=a*b*c/100
+print(d)
---
+++ Q.55.py
@@ -0,0 +1,5 @@
+a=int(input("Enter Year : "))
+if a%4==0:
+    print("Leap Year")
+else:
+    print("Not Leap year")
